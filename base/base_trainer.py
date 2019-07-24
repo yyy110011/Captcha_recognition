26573cfb186f917a25cf0e2443263f8d8ac533b0
ZRYao <yyy110011@livemail.tw>
2019-07-24 14:18:11 +0800

    Add class: base, simple_captcha_model, simple_trainer

---
+++ base/base_trainer.py
@@ -0,0 +1,7 @@
+class BaseTrainer(object):
+    def __init__(self, model, data, config):
+        self.model = model
+        self.data = data
+        self.config = config
+    def train(self):
+        raise NotImplementedError
---
+++ trainers/simple_trainer.py
@@ -0,0 +1,5 @@
+from base.base_trainer import BaseTrainer
+
+
+class simple_trainer(BaseTrainer):
+    
