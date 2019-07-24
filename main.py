6627c772b4850cdbacbab2cf989e40bb1a1eeccf
ZRYao <yyy110011@livemail.tw>
2019-07-24 19:33:06 +0800

    Modify main.py

--- main.py
+++ main.py
@@ -6,4 +6,17 @@ from config import config
 def main():
 
     cfg = config()
-    data
+    print('Loading Data...')
+    data_loader = CaptchaDataLoader(cfg)
+
+    print('Loading Model...')
+    model = SimpleCaptchaModel(cfg)
+
+    print('Loading Trainer...')
+    trainer = SimpleTrainer(model.model, data_loader.get_train_data(), cfg)
+
+    trainer.train()
+
+
+if if __name__ == "__main__":
+    main()
