931049c631443f9b3ba81728663ed5f73e19834c
ZRYao <yyy110011@livemail.tw>
2019-07-24 17:13:18 +0800

    modified config to dotmap, simple_trainer.py

--- config.py
+++ config.py
@@ -1,6 +1,23 @@
-class config():
-    def __init__(self):
-        self.useCircle = 1
-        self.loss = 'categorical_crossentropy'
-        self.optimizer = 'Adamax'
-        self.metrics = ['accuracy']
+from dotmap import DotMap
+import time
+
+def config():
+
+    m = DotMap()
+    m.useCircle = 1
+    m.data_path = 'label.txt'
+
+    m.trainer.batch_size = 128
+    m.trainer.num_epochs = 200
+    m.trainer.validation_split = .2
+    m.trainer.verbose_training = 1
+
+
+    m.model.loss = 'categorical_crossentropy'
+    m.model.optimizer = 'Adamax'
+    m.model.metrics = ['accuracy']
+
+    now = time.strftime("%y%m%d_%H:%M:%S")
+    m.callbacks.tensorboard_log_dir = './logs/'+now + '_e' + str(m.trainer.num_epochs) + '_b' + str(m.trainer.batch_size)
+
+
--- trainers/simple_trainer.py
+++ trainers/simple_trainer.py
@@ -21,6 +21,29 @@ class SimpleCaptchaTrainer(BaseTrainer):
         self.callbacks.append(
             TensorBoard(
                 log_dir=self.config.callbacks.tensorboard_log_dir,
-                write_graph=self.config.callbacks.tensorboard_write_graph,
+                #write_graph=self.config.callbacks.tensorboard_write_graph,
             )
         )
+
+    def train(self):
+        history = self.model.fit(
+            self.data[0], self.data[1],
+            epochs=self.config.trainer.num_epochs,
+            verbose=self.config.trainer.verbose_training,
+            batch_size=self.config.trainer.batch_size,
+            validation_split=self.config.trainer.validation_split,
+            callbacks=self.callbacks,
+        )
+        self.loss.extend(history.history['loss'])
+        self.acc.extend(history.history['acc'])
+        self.val_loss_1.extend(history.history['val_digit1_loss'])
+        self.val_acc_1.extend(history.history['val_digit1_acc'])
+
+        self.val_loss_2.extend(history.history['val_digit2_loss'])
+        self.val_acc_2.extend(history.history['val_digit2_acc'])
+
+        self.val_loss_3.extend(history.history['val_digit3_loss'])
+        self.val_acc_3.extend(history.history['val_digit3_acc'])
+
+        self.val_loss_4.extend(history.history['val_digit4_loss'])
+        self.val_acc_4.extend(history.history['val_digit4_acc'])
